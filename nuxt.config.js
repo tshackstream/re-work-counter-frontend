4bd802c9755a53850f8435dd053670a3ae5e7bef
tshackstream <takuya_sakai@ts-hackstream.com>
2020-11-29 18:07:01 +0900

    下準備

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: '@/plugins/vee-validate' }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -65,5 +65,12 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    transpile: ['vee-validate/dist/rules'],
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.devtool = 'eval-source-map'
+      }
+    },
+  },
 }
---
+++ plugins/vee-validate.ts
@@ -0,0 +1,22 @@
+// cf. https://www.virment.com/how-to-use-veevalidate-nuxtjs/
+
+import Vue from 'vue'
+import {
+  ValidationProvider,
+  ValidationObserver,
+  localize,
+  extend,
+} from 'vee-validate'
+import ja from 'vee-validate/dist/locale/ja.json'
+import * as rules from 'vee-validate/dist/rules'
+
+// with typescript
+for (const [rule, validation] of Object.entries(rules)) {
+  extend(rule, {
+    ...validation,
+  })
+}
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
+localize('ja', ja)
